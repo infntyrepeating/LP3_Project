dc9faff440ac91991aa5339477586ad67f4ec590
infntyrepeating <infntyrepeating@gmail.com>
2024-04-30 01:58:44 +0100

    Initial commit

---
+++ Source/LP3_Project/LP3_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LP3_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LP3_Project, "LP3_Project" );
